521688c0b74209632eb1410b651f1d9c9867c488
mingTien <saijvu6766@gmail.com>
2022-11-17 20:53:25 +0700

    Minh tien gui database

---
+++ DuAn1.sql
@@ -0,0 +1,110 @@
+﻿Create Database DuAn1
+Use DuAn1
+--Màu Sắc
+Create Table MauSac(
+Id INT Identity(1,1) primary key,
+Ten nvarchar(50) not  null
+)
+--Kích cỡ
+Create Table KichCo(
+Id INT Identity(1,1) primary key,
+Ten nvarchar(50) not  null
+)
+--Chất liệu
+Create Table ChatLieu(
+Id INT Identity(1,1) primary key,
+Ten nvarchar(50) not  null
+)
+--Danh Mục sản phẩm
+Create Table DanhMucSP(
+Id INT Identity(1,1) primary key,
+Ten nvarchar(50) not null
+)
+--Thương hiệu
+Create Table ThuongHieu(
+Id INT Identity(1,1) primary key,
+Ten nvarchar(50) not null
+)
+--Nhà sản xuất
+Create Table NSX(
+Id INT Identity(1,1) primary key,
+Ten nvarchar(50) not null
+)
+--Chức vụ
+Create Table ChucVu(
+Id INT Identity(1,1) primary key,
+Ten nvarchar(50) not null
+)
+--Khuyến mãi
+Create Table KhuyenMai(
+Id INT Identity(1,1) primary key,
+Ten nvarchar(50) not null,
+Ngaybatdau Date not null,
+Ngayketthuc date not null,
+HinhthucKM nvarchar(50) not null,
+Giatrigiam decimal DEFAULT 0
+)
+
+--Chi tiết sản phẩm
+Create Table ChitietSP(
+Id INT Identity(1,1) PRIMARY KEY ,
+Ma nvarchar(10) not  null,
+Ten nvarchar(50) not null,
+IdNsx INT foreign key references NSX(Id),
+IdMauSac INT foreign key references MauSac(Id),
+IdDMuc INT foreign key references DanhMucSP(Id),
+IdKC INT foreign key references KichCo(Id),
+IdCL INT foreign key references ChatLieu(Id),
+IdTH INT foreign key references ThuongHieu(Id),
+IdKM INT foreign key references KhuyenMai(Id),
+MoTa NVARCHAR(50) DEFAULT NULL,
+SoLuongTon INT,
+GiaNhap DECIMAL(20,0) DEFAULT 0,
+GiaBan DECIMAL(20,0) DEFAULT 0,
+Hinhanh varchar(max) null
+)
+--Nhân viên
+Create Table Users(
+Id INT Identity(1,1) PRIMARY KEY ,
+Ten NVARCHAR(30) not null,
+TenDem NVARCHAR(30) DEFAULT NULL,
+Ho NVARCHAR(30) DEFAULT NULL,
+NgaySinh DATE DEFAULT NULL,
+Gioitinh bit null,
+Sdt VARCHAR(30) DEFAULT NULL,
+IdCV INT foreign key references ChucVu(Id),
+TaiKhoan VARCHAR(MAX) DEFAULT NULL,
+MatKhau VARCHAR(MAX) DEFAULT NULL,
+Email varchar(max) not null,
+TrangThai bit DEFAULT 0
+)
+--Khách hàng
+Create Table KhachHang(
+Id INT Identity(1,1) PRIMARY KEY ,
+Ten NVARCHAR(30) not null,
+TenDem NVARCHAR(30) DEFAULT NULL,
+Ho NVARCHAR(30) DEFAULT NULL,
+Gioitinh bit Null,
+NgaySinh DATE DEFAULT NULL,
+Email varchar(max) not null,
+Sdt VARCHAR(30) DEFAULT NULL,
+Diemthuong int DEFAULT NULL
+)
+--Hóa đơn
+Create Table HoaDon(
+Id INT Identity(1,1) primary key ,
+IdKH INT foreign key references KhachHang(Id),
+IdNV INT foreign key references Users(Id),
+Ma VARCHAR(20) UNIQUE,
+NgayTao DATE DEFAULT NULL,
+NgayThanhToan DATE DEFAULT NULL,
+TinhTrang bit DEFAULT 0
+)
+--Hóa đơn chi tiết
+Create Table HoaDonChiTiet(
+IdHD int foreign key references HoaDon(Id) not null,
+IdCTSP int foreign key references ChitietSP(Id) not null,
+Soluong int not  null,
+Dongia decimal not  null,
+primary key(IdHD,IdCTSP)
+)
