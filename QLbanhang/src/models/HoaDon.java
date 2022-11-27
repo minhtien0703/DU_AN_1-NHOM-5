5217ecb7a41af264e7f29f9fd0279b07449d54f1
toanduy1410 <toanndph25206@fpt.edu.vn>
2022-11-28 00:04:30 +0700

    no message

--- QLbanhang/src/models/HoaDon.java
+++ QLbanhang/src/models/HoaDon.java
@@ -1,7 +1,4 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package models;
 
 import java.sql.Date;
@@ -11,36 +8,51 @@ import java.sql.Date;
  * @author PC
  */
 public class HoaDon {
+
+     private Integer id;
+    private Integer IDKH;
+    private Integer IDNV;
     private String Ma;
     private Date NgayTao;
     private Date NgayThanhToan;
-    private int TinhTrang;
-    private String GhiChu;
+    private double TinhTrang;
 
     public HoaDon() {
     }
-    
-   
 
-    public HoaDon(String Ma, Date NgayTao, Date NgayThanhToan, int TinhTrang,String GhiChu) {
+    public HoaDon(Integer id, Integer IDKH, Integer IDNV, String Ma, Date NgayTao, Date NgayThanhToan, int TinhTrang) {
+        this.id = id;
+        this.IDKH = IDKH;
+        this.IDNV = IDNV;
         this.Ma = Ma;
         this.NgayTao = NgayTao;
         this.NgayThanhToan = NgayThanhToan;
         this.TinhTrang = TinhTrang;
-        this.GhiChu = GhiChu;
     }
 
-    public int getId() {
+    public Integer getId() {
         return id;
     }
 
-    public void setId(int id) {
+    public void setId(Integer id) {
         this.id = id;
     }
-    
-   
 
-    
+    public Integer getIDKH() {
+        return IDKH;
+    }
+
+    public void setIDKH(Integer IDKH) {
+        this.IDKH = IDKH;
+    }
+
+    public Integer getIDNV() {
+        return IDNV;
+    }
+
+    public void setIDNV(Integer IDNV) {
+        this.IDNV = IDNV;
+    }
 
     public String getMa() {
         return Ma;
@@ -66,22 +78,23 @@ public class HoaDon {
         this.NgayThanhToan = NgayThanhToan;
     }
 
-    public int getTrangThai() {
-        return TrangThai;
+    public double getTinhTrang() {
+        return TinhTrang;
     }
 
-    public void setTrangThai(int TrangThai) {
-        this.TrangThai = TrangThai;
+    public void setTinhTrang(double TinhTrang) {
+        this.TinhTrang = TinhTrang;
     }
 
     @Override
     public String toString() {
-        return "HoaDon{" + "id=" + id + ", IDKH=" + IDKH + ", IDNV=" + IDNV + ", Ma=" + Ma + ", NgayTao=" + NgayTao + ", NgayThanhToan=" + NgayThanhToan + ", TrangThai=" + TrangThai + '}';
+        return "HoaDon{" + "id=" + id + ", IDKH=" + IDKH + ", IDNV=" + IDNV + ", Ma=" + Ma + ", NgayTao=" + NgayTao + ", NgayThanhToan=" + NgayThanhToan + ", TinhTrang=" + TinhTrang + '}';
     }
 
-    public void setGhiChu(String GhiChu) {
-        this.GhiChu = GhiChu;
-    }
+   
 
     
+     
+
 }
+ 
