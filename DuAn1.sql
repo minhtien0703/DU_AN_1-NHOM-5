0d047ab7b01083391688c48fcace1b7741709068
mingTien0703 <saijvu6766@gmail.com>
2022-11-28 12:51:19 +0700

    Tiến thêm thuộc tính trạng thái cho khuyến mãi

--- DuAn1.sql
+++ DuAn1.sql
@@ -88,12 +88,8 @@ MatKhau VARCHAR(MAX) DEFAULT NULL,
 Email varchar(max) not null,
 TrangThai bit DEFAULT 0
 )
-
---Khách hàng
-
-
----
 go
+--Khách hàng
 Create Table KhachHang(
 Id INT Identity(1,1) PRIMARY KEY ,
 Ten NVARCHAR(30) not null,
@@ -129,72 +125,6 @@ Dongiakhigiam decimal not  null
 ALTER TABLE Hoadon
 
 ADD Ghichu varchar(255)
+ALTER TABLE Khuyenmai
 
-insert into ThuongHieu values(N'CHÂU ÂU')
-insert into ThuongHieu values(N'CHÂU PHI')
-insert into ThuongHieu values(N'ARIDAT')
-
-select * from ThuongHieu
-
-insert into NSX values(N'Công Ty TNHH Sơn Tùng')
-insert into NSX values(N'Công Ty TNHH Thiết Kế Thời Trang Tân Mỹ')
-insert into NSX values(N'Công Ty TNHH ANMAC Việt Nam')
-
-select * from NSX
-
-insert into ChucVu values(N'nhân viên')
-insert into ChucVu values(N'quản lý')
-
-select * from ChucVu
-
-insert into DanhMucSP values(N'GIÁ TRỊ CAO')
-insert into DanhMucSP values(N'GIÁ TRỊ THẤP')
-insert into DanhMucSP values(N'GIÁ TRỊ BÌNH THƯỜNG')
-
-select * from DanhMucSP
-
-insert into ChatLieu values(N'cattong')
-insert into ChatLieu values(N'nỉ')
-insert into ChatLieu values(N'giấy')
-
-select * from ChatLieu
-
-insert into KichCo values('sai30')
-insert into KichCo values('sai31')
-insert into KichCo values('sai32')
-
-select * from KichCo
-
-insert into MauSac values(N'MÀU ĐỎ')
-insert into MauSac values(N'MÀU CAM')
-insert into MauSac values(N'MÀU HỒNG')
-
-SELECT * FROM MauSac
-
-insert into KhachHang values(N'TOẢN ĐB',N'VĂN',N'NGUYỄN',0,'2003-05-05','toandbnvph25194@fpt.edu.vn','0968659481',null)
-insert into KhachHang values(N'DUY ĐB',N'HỮU',N'NGUYỄN',0,'2003-10-23','duydbnvph25345@fpt.edu.vn','0968659481',null)
-
-select * from KhachHang
-
-
-insert into Users values(N'CHIẾN ĐB',N'VĂN',N'NGUYỄN','2003-05-05',0,'0968659481',null,N'CHIEN01','123456789','CHIENdbnvph25345@fpt.edu.vn',NULL)
-insert into Users values(N'HUY ĐB',N'DUY',N'NGUYỄN','2003-05-06',0,'0968659481',null,N'HUY01','123456789','HUYdbnvph25345@fpt.edu.vn',NULL)
-
-select * from Users
-
-
-insert into HoaDon values(2,1,'1','2003-04-05','2003-02-05',0,null)
-insert into HoaDon values(3,2,'2','2003-06-15','2003-06-15',0,null)
-
-select * from HoaDon
-
-insert into KhuyenMai values(N'toản','2003-04-05','2003-04-05','sale',0)
-insert into KhuyenMai values(N'huy','2003-04-05','2003-04-05',N'kh cũ',1)
-
-select * from KhuyenMai
-
-
-insert into HoaDonChiTiet values(N'toản','2003-04-05','2003-04-05','sale',0)
-
-select * from HoaDonChiTiet
-
+ADD Trangthai bit Default 0
